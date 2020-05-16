61125fdc6083e071445ba2d1ff4e44bc2d0d57d7
Gasper Stipanic <gasper.stipanic@epilog.net>
2020-05-16 19:06:25 +0200

    Added classes for handling Sony and Siol IR commands

---
+++ Beomote/Sony.cpp
@@ -0,0 +1,48 @@
+#include "Sony.h"
+#include "IRremote.h"
+#include "Beomote.h"
+
+void Sony::sendCommand(SonyCode code, int numberOfBits = 12) {
+    for (int i = 0; i < 4; i++) {
+      ir.sendSony(code, numberOfBits);
+      delay(40);
+  }
+}
+
+
+void Sony::handleCommand(BeoCommand cmd) {
+
+  if (cmd.command == UP) {
+      Serial.println("UP");
+      sendCommand(SONY_UP, 12);
+  }
+  else if (cmd.command == DOWN) {
+    Serial.println("DOWN");
+    sendCommand(SONY_DOWN, 12);
+  }
+  else if (cmd.command == LEFT) {
+    Serial.println("LEFT");
+    sendCommand(SONY_LEFT, 12);
+  }
+  else if (cmd.command == RIGHT) {
+    Serial.println("RIGHT");
+    sendCommand(SONY_RIGHT, 12);
+  }
+  else if (cmd.command == GO) {
+    Serial.println("OK (GO)");
+    sendCommand(SONY_OK, 12);
+  }
+  else if (cmd.command == RED) {
+    Serial.println("BACK (RED)");
+    sendCommand(SONY_BACK, 15);
+  }
+  else if (cmd.command == BLUE) {
+    Serial.println("HOME (BLUE)");
+    sendCommand(SONY_HOME, 12);
+  }
+  else if (cmd.command == GREEN) {
+    Serial.println("TV (GREEN)");
+    sendCommand(SONY_TV, 12);
+  }
+    
+}
